09be1924f7116c07db2d467e97cc74c02a71355c
Sagar V <1rn17cs079.sagarv@gmail.com>
2024-12-30 20:39:19 +0530

    feat: dealing with basic functions;

--- main.go
+++ main.go
@@ -2,51 +2,16 @@ package main
 
 import "fmt"
 
-type Product struct {
-	id    int64
-	title string
-	price float64
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	doubled := doubleNumbers(&numbers)
+	fmt.Println(doubled)
 }
 
-func main() {
-	// Working with arrays
-	// Empty array => hobbies := []string{}
-	hobbies := []string{"Music", "Coding", "Gaming"}
-	fmt.Println(hobbies)
-	// Playing with slices
-	fmt.Println(hobbies[0])
-	fmt.Println(hobbies[1:])
-	fmt.Println(cap(hobbies))
-	hobbies = append(hobbies, "Upskilling")
-	fmt.Println(hobbies)
-
-	// Working with struct arrays
-	products := []Product{
-		{
-			id:    1,
-			title: "Product 1",
-			price: 100.00,
-		},
-		{
-			id:    2,
-			title: "Product 2",
-			price: 200.00,
-		},
+func doubleNumbers(n *[]int) []int {
+	dNum := []int{}
+	for _, v := range *n {
+		dNum = append(dNum, v*2)
 	}
-	thirdProduct := Product{
-		id:    3,
-		title: "Product 3",
-		price: 300.00,
-	}
-	products = append(products, thirdProduct)
-	fmt.Println(products)
-
-	// Working with maps
-	// Empty map => person := map[string]string{}
-	person := map[string]string{
-		"first_name": "John",
-		"last_name":  "Doe",
-		"email_id":   "john_doe@gmail.com",
-	}
-	fmt.Println(person)
+	return dNum
 }
