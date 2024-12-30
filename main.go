d255b446ce9296a0e201c3ded80f85e9d381e6ac
Sagar V <1rn17cs079.sagarv@gmail.com>
2024-12-30 16:10:09 +0530

    chore: Playing with arrays and struct arrays

--- main.go
+++ main.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Print("Hello World!\n")
+type Product struct {
+	id    int64
+	title string
+	price float64
+}
+
+func main() {
+	// Working with arrays
+	hobbies := []string{"Music", "Coding", "Gaming"}
+	fmt.Println(hobbies)
+	// Playing with slices
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
+	fmt.Println(cap(hobbies))
+	hobbies = append(hobbies, "Upskilling")
+	fmt.Println(hobbies)
+
+	// Working with struct arrays
+	products := []Product{
+		{
+			id:    1,
+			title: "Product 1",
+			price: 100.00,
+		},
+		{
+			id:    2,
+			title: "Product 2",
+			price: 200.00,
+		},
+	}
+	products = append(products, Product{
+		id:    3,
+		title: "Product 3",
+		price: 300.00,
+	})
+	fmt.Println(products)
 }
