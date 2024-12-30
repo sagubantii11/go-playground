4761a83d7c92e1658aff2a5c116a2f377b11dac8
Sagar V <1rn17cs079.sagarv@gmail.com>
2024-12-30 21:47:17 +0530

    feat: Implement factorial calculation and refactor main to use factorial function

---
+++ factorial/factorial.go
@@ -0,0 +1,21 @@
+package factorial
+
+import "fmt"
+
+func FactorialMain() {
+	var number int
+	fmt.Print("Enter a number: ")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+	fmt.Println("Factorial of", number, "is => ", factorial(number))
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
--- main.go
+++ main.go
@@ -1,44 +1,7 @@
 package main
 
-import "fmt"
-
-type fInt func(int) int
+import "github.com/sagubantii11/go-playground/factorial"
 
 func main() {
-	// Working with Anonymous Functions
-	numbers := []int{1, 2, 3, 4, 5}
-	doubled := transformNumbers(&numbers, func(num int) int { return num * 2 })
-	fmt.Println("Doubled: ", doubled)
-	tripled := transformNumbers(&numbers, func(num int) int { return num * 3 })
-	fmt.Println("Tripled: ", tripled)
-	squared := transformNumbers(&numbers, func(num int) int { return num * num })
-	fmt.Println("Squared: ", squared)
-	
-	fmt.Println("Doubled again: ", transformNumbers(&doubled, createFunc(2))) // Usage of a function factory
+	factorial.FactorialMain()
 }
-
-func transformNumbers(num *[]int, transform fInt) []int {
-	dNum := []int{}
-	for _, v := range *num {
-		dNum = append(dNum, transform(v))
-	}
-	return dNum
-}
-
-func createFunc(factor int) fInt {
-	return func(num int) int {
-		return num * factor
-	}
-}
-
-// func double(num int) int {
-// 	return num * 2
-// }
-
-// func triple(num int) int {
-// 	return num * 3
-// }
-
-// func square(num int) int {
-// 	return num * num
-// }
