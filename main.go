8faf703d26947ba87b9e3d34a1abff1321bfae83
Sagar V <1rn17cs079.sagarv@gmail.com>
2024-12-30 20:52:34 +0530

    feat: Refactor number transformation functions for flexibility, by passing functions as values

--- main.go
+++ main.go
@@ -4,14 +4,30 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	doubled := doubleNumbers(&numbers)
-	fmt.Println(doubled)
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println("Doubled: ", doubled)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println("Tripled: ", tripled)
+	squared := transformNumbers(&numbers, square)
+	fmt.Println("Squared: ", squared)
 }
 
-func doubleNumbers(n *[]int) []int {
+func transformNumbers(n *[]int, transform func(int) int) []int {
 	dNum := []int{}
 	for _, v := range *n {
-		dNum = append(dNum, v*2)
+		dNum = append(dNum, transform(v))
 	}
 	return dNum
+}
+
+func double(num int) int {
+	return num * 2
+}
+
+func triple(num int) int {
+	return num * 3
+}
+
+func square(num int) int {
+	return num * num
 }
