08552a41ca28f1ba8d23f896521f645ee2eca997
Sagar V <1rn17cs079.sagarv@gmail.com>
2024-12-30 21:12:11 +0530

    feat: Update transformNumbers to use anonymous functions for transformations

--- main.go
+++ main.go
@@ -2,32 +2,35 @@ package main
 
 import "fmt"
 
+type fInt func(int) int
+
 func main() {
+	// Working with Anonymous Functions
 	numbers := []int{1, 2, 3, 4, 5}
-	doubled := transformNumbers(&numbers, double)
+	doubled := transformNumbers(&numbers, func(num int) int { return num * 2 })
 	fmt.Println("Doubled: ", doubled)
-	tripled := transformNumbers(&numbers, triple)
+	tripled := transformNumbers(&numbers, func(num int) int { return num * 3 })
 	fmt.Println("Tripled: ", tripled)
-	squared := transformNumbers(&numbers, square)
+	squared := transformNumbers(&numbers, func(num int) int { return num * num })
 	fmt.Println("Squared: ", squared)
 }
 
-func transformNumbers(n *[]int, transform func(int) int) []int {
+func transformNumbers(num *[]int, transform fInt) []int {
 	dNum := []int{}
-	for _, v := range *n {
+	for _, v := range *num {
 		dNum = append(dNum, transform(v))
 	}
 	return dNum
 }
 
-func double(num int) int {
-	return num * 2
-}
+// func double(num int) int {
+// 	return num * 2
+// }
 
-func triple(num int) int {
-	return num * 3
-}
+// func triple(num int) int {
+// 	return num * 3
+// }
 
-func square(num int) int {
-	return num * num
-}
+// func square(num int) int {
+// 	return num * num
+// }
